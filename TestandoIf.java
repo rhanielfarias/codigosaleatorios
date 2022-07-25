f49520694d9ccf2022946aa9795e642f29e7790a
rhanielfarias <rhanielfarias@gmail.com>
2022-07-25 18:06:43 -0300

    oi

---
+++ TestandoIf.java
@@ -0,0 +1,23 @@
+public class TestandoIf {
+    public static void main(String[] args) {
+        int idade = 20;
+        int quantidadePessoa = 1;
+        // boolean acompanhar = quantidadePessoa <= 2;
+        boolean acompanhar;
+        System.out.println(idade);
+        if(idade >= 18 && quantidadePessoa <= 2) {
+            acompanhar = false;
+        } else  {
+            acompanhar = true;
+        }
+     if(idade >= 18 && acompanhar) {
+         System.out.println("Seja bem-vindo");
+     } else {
+         System.out.pr  intln("Infelizmente você não pode entrar");
+         System.out.println("Infelizmente você não pode entrar");
+         System.out.println("Infelizmente você não pode entrar");
+     }
+     }
+    }
+
+
